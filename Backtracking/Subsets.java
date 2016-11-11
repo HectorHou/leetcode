4b846b5787276f9fb96641cde8e618a76124199e
HectorHou <houming1993@foxmail.com>
2016-11-11 18:32:18 +0800

    solution of 78. Subsets and 90. Subsets II

---
+++ Backtracking/Subsets.java
@@ -0,0 +1,42 @@
+// 78. Subsets   QuestionEditorial Solution  My Submissions
+// Total Accepted: 126242
+// Total Submissions: 354955
+// Difficulty: Medium
+// Contributors: Admin
+// Given a set of distinct integers, nums, return all possible subsets.
+//
+// Note: The solution set must not contain duplicate subsets.
+//
+// For example,
+// If nums = [1,2,3], a solution is:
+//
+// [
+//   [3],
+//   [1],
+//   [2],
+//   [1,2,3],
+//   [1,3],
+//   [2,3],
+//   [1,2],
+//   []
+// ]
+
+public class subsets {
+  /*
+  *Solution : Backtracking
+  */
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> result = new LinkedList<>();
+        List<Integer> list = new LinkedList<>();
+        helper(result, list, nums, 0);
+        return result;
+    }
+    private void helper(List<List<Integer>> result, List<Integer> list, int[] nums, int start){
+        result.add(new ArrayList(list));
+        for (int i = start; i < nums.length; i++){
+            list.add(nums[i]);
+            helper(result, list, nums, i + 1);
+            list.remove(list.size() - 1);
+        }
+    }
+}
---
+++ Backtracking/SubsetsII.java
@@ -0,0 +1,43 @@
+// 90. Subsets II   QuestionEditorial Solution  My Submissions
+// Total Accepted: 87238
+// Total Submissions: 262972
+// Difficulty: Medium
+// Contributors: Admin
+// Given a collection of integers that might contain duplicates, nums, return all possible subsets.
+//
+// Note: The solution set must not contain duplicate subsets.
+//
+// For example,
+// If nums = [1,2,2], a solution is:
+//
+// [
+//   [2],
+//   [1],
+//   [1,2,2],
+//   [2,2],
+//   [1,2],
+//   []
+// ]
+
+public class Solution {
+  /*
+  *Solution : Prevent the duplicate subsets.
+  *            We First sort the nums and for every iteration we only add the same numbers once.
+  */
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        List<List<Integer>> result = new LinkedList<>();
+        helper(result, new LinkedList<Integer>(), nums, 0);
+        return result;
+    }
+    private void helper(List<List<Integer>> result, List<Integer> list, int[] nums, int start){
+        result.add(new ArrayList(list));
+        for (int i = start; i < nums.length; i++){
+            list.add(nums[i]);
+            helper(result, list, nums, i + 1);
+            list.remove(list.size() - 1);
+            while (i < nums.length - 1 && nums[i] == nums[i + 1])
+                i++;
+        }
+    }
+}
